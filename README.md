8061a84ecce5524fa13df7b96364bdd3086a0162
Annadi <annadirv@thoughtworks.com>
2021-07-29 11:13:45 +0530

    [Sezzle]Annadi: add read me to run the app

---
+++ README.md
@@ -0,0 +1,14 @@
+# Calculator
+computes all expressions with BODMAS rule
+
+
+Steps to run the application:
+
+` ./gradlew clean build  `
+
+` java -jar calculator-app-1.0-SNAPSHOT.jar "<< expression to compute >>"`
+ 
+**example:**
+
+
+`java -jar build/libs/calculator-app-1.0-SNAPSHOT.jar "(2+3)*3"`
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -25,37 +25,11 @@ public class Calculator {
             for (int i = 0; i < expr.length(); i++) {
                 char current = expr.charAt(i);
                 if (current >= '0' && current <= '9') {
-                    StringBuilder builder = new StringBuilder();
-                    builder.append(current);
-                    i++;
-                    while (i < expr.length() && ((expr.charAt(i) >= '0' && expr.charAt(i) <= '9') || expr.charAt(i) == '.')) {
-                        builder.append(expr.charAt(i++));
-                    }
-                    i--;
-                    values.add(Double.parseDouble(builder.toString()));
+                    i = readNumber(i, current);
                 } else if (current == '(') {
-                    int openbraces = 1;
-                    StringBuilder builder = new StringBuilder();
-                    i++;
-                    while (i < expr.length() && openbraces > 0) {
-                        if (expr.charAt(i) == '(') {
-                            openbraces++;
-                        }
-                        if (expr.charAt(i) == ')') {
-                            openbraces--;
-                        }
-                        if (openbraces >= 1)
-                            builder.append(expr.charAt(i++));
-                    }
-                    values.push(new Calculator(builder.toString()).evaluate());
+                    i = dealWithExprWithInBraces(i);
                 } else {
-                    Operator operator = factory.with(current);
-                    while (!operators.isEmpty() && operator.level() >= operators.peek().level()) {
-                        double compute = operators.pop().calculate(values.pop(), values.pop());
-                        values.add(compute);
-                    }
-
-                    operators.add(operator);
+                    calculateAllLeftHigherLevelExpr(current);
                 }
             }
 
@@ -69,6 +43,46 @@ public class Calculator {
             throw new InvalidExpressionException("more number of operators added", e);
         }
 
+    }
+
+    private int readNumber(int i, char current) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(current);
+        i++;
+        while (i < expr.length() && ((expr.charAt(i) >= '0' && expr.charAt(i) <= '9') || expr.charAt(i) == '.')) {
+            builder.append(expr.charAt(i++));
+        }
+        i--;
+        values.add(Double.parseDouble(builder.toString()));
+        return i;
+    }
+
+    private void calculateAllLeftHigherLevelExpr(char current) throws InvalidExpressionException {
+        Operator operator = factory.with(current);
+        while (!operators.isEmpty() && operator.level() >= operators.peek().level()) {
+            double compute = operators.pop().calculate(values.pop(), values.pop());
+            values.add(compute);
+        }
+
+        operators.add(operator);
+    }
+
+    private int dealWithExprWithInBraces(int i) throws InvalidExpressionException {
+        int openbraces = 1;
+        StringBuilder builder = new StringBuilder();
+        i++;
+        while (i < expr.length() && openbraces > 0) {
+            if (expr.charAt(i) == '(') {
+                openbraces++;
+            }
+            if (expr.charAt(i) == ')') {
+                openbraces--;
+            }
+            if (openbraces >= 1)
+                builder.append(expr.charAt(i++));
+        }
+        values.push(new Calculator(builder.toString()).evaluate());
+        return i;
     }
 
 }
